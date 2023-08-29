3ecec050cb3ae5e78db74c2f19be1cf2ad73a917
Anderson Silva <arobertok@gmail.com>
2023-08-29 16:21:32 -0300

    Implementação da comunicação entre msavaliacaocredito e mscartoes

--- msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,10 +1,14 @@
 package io.github.cursodsousa.msavaliadorcredito.application;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import io.github.cursodsousa.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.cursodsousa.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.cursodsousa.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.cursodsousa.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import io.github.cursodsousa.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 
@@ -13,13 +17,16 @@ import lombok.RequiredArgsConstructor;
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clientesClient;
+    private final CartoesResourceClient cartoesClient;
     
     public SituacaoCliente obterSituacaoCliente(String cpf){
         ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
                 .build();
     }
 }
